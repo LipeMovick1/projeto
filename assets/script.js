7b0626234297702bbc8beaf7a918ea56e8e78829
andre philipe <philipe65@gmail.com>
2026-08-18 14:00:26 -0300

    :tada: initial commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+function togglemode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  //pegar a tag da imagem
+  const img = document.querySelector("#profile img")
+  //substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./avatar-light.png")
+  } else {
+    // se tiver sem light mode, adicionar  a imagem normal
+    img.setAttribute("src", "./Andre.jpg")
+  }
+}
+
+// if (html.classList.contains("light")) {
+//   html.classList.remove("light")
+//} else {
+//   html.classList.add("light")
+// }
